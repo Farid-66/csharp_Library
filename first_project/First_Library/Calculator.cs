0e0869c94abe4b307f92081a19afd0fc605bd950
Farid-66 <feridquba66@gmail.com>
2021-11-03 10:45:34 -0700

    Task_Library

---
+++ first_project/First_Library/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace First_Library
+{
+    public class Calculator
+    {
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+        public int Dif(int a, int b)
+        {
+            return a - b;
+        }
+        public int Mult(int a, int b)
+        {
+            return a * b;
+        }
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Pow(int a, int b)
+        {
+            int c = 1;
+            for(int i = 0; i < b; i++)
+            {
+               c *= a;
+            }
+            return c;
+        }
+    }
+}
---
+++ first_project/first_project/Program.cs
@@ -0,0 +1,25 @@
+﻿using First_Library;
+using System;
+
+namespace first_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator solve = new();
+
+            Console.Write("Enter first number:  ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number:  ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine($"{a}+{b}="+solve.Add(a,b));
+            Console.WriteLine($"{a}-{b}="+solve.Dif(a,b));
+            Console.WriteLine($"{a}*{b}="+solve.Mult(a,b));
+            Console.WriteLine($"{a}/{b}="+solve.Div(a,b));
+            Console.WriteLine($"{a}^{b}="+solve.Pow(a,b));
+        }
+    }
+}
